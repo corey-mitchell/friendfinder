0ee73e1e9922f3233af0b900f76377a871503ff2
corey-mitchell <corey69_96@yahoo.com>
2018-09-03 17:44:50 -0500

    edited some html, set up html routes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+const friendsList = [
+    {
+        name: 'Johnny Watts',
+        photo: '',
+        scores: [5, 3, 2, 4, 1, 4, 3, 5, 3, 2]
+    }
+];
+
+module.exports = friendsList;
--- server.js
+++ server.js
@@ -5,26 +5,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(`${__dirname}/app/public/home.html`))
-});
-
-app.get('/survey', (req, res)=>{
-    res.sendFile(path.join(`${__dirname}/app/public/survey.html`))
-});
-
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }));
  
 // parse application/json
-app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+app.use(bodyParser.json());
+
+// imports page routes from htmlRoutes file
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, ()=>{
-    console.log(`App listening on PORT: ${PORT}`)
+    console.log(`App listening on PORT: ${PORT}`);
 });
